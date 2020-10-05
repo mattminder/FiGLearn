f517aea962b17082eed8863068b7469d898178a9
mattminder <myfiles@Mattus-MBP.lan>
2020-10-05 09:26:31 +0200

    Changed neural net default parameters to have more hidden layers, changed function to softmax

--- NNet.py
+++ NNet.py
@@ -3,7 +3,7 @@ import torch
 class NNet(torch.nn.Module):
     
     
-    def __init__(self, n_hidden=3, h_size=10):
+    def __init__(self, n_hidden=5, h_size=30):
         super().__init__()
         modules = []
         modules.append(torch.nn.Linear(1, h_size))
@@ -13,11 +13,14 @@ class NNet(torch.nn.Module):
             modules.append(torch.nn.ReLU())
             
         self.hidden = torch.nn.Sequential(*modules)
-        self.last = torch.nn.Linear(h_size, 1, bias=False)
+        self.last = torch.nn.Linear(h_size, 1, bias=True)
+        self.softplus = torch.nn.Softplus()
+        
         
     def forward(self, x, tol=1e-3):
         hid = self.hidden(x)
         logout = self.last(hid)
         val = torch.exp(-logout)
-        return val
-    
+        #val = torch.nn.ReLU(logout)
+        #return val
+        return self.softplus(logout)
